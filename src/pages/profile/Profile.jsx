c82b9eac0e08b140660d0603bb1448a034d258a2
jvimperial <joshuaimperial1010@gmail.com>
2025-04-06 17:08:53 +0800

    Profile Settings Layout

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -22,12 +22,149 @@ export default function Profile(){
     return(
 
         <>  
-            <div class="main-cont-prof-settings">
-                <div class="header-cont-prof-settings">
-                    <h1 class="header-text-prof-settings">Profile Settings</h1>
-                </div>
+            <div className="main-cont-prof-settings">
 
-                <div class="content-cont">
+                <div className='header-container-prof-settings'>
+                    <h1 className='header-text-prof-settings'>Profile Settings</h1>
+                </div>        
+
+                <div className='content-container-prof-settings'>
+
+                    <div className='avatar-edit-container-prof-settings'>
+
+                        <div className='avatar-container-prof-settings'>
+                            {image ? <img class="avatar-dimensions" src={URL.createObjectURL(image)} alt=""/> : <img class="avatar-dimensions" src="https://avatarfiles.alphacoders.com/375/375159.jpeg" alt="" />}
+                            <input 
+                            type='file' 
+                            ref={inputRef} 
+                            onChange={handleImageChange} 
+                            style={{display: "none"}}
+                            accept='image/*'
+                            />
+                        </div>
+
+                        <div className='edit-btn-container-prof-settings'>
+                            <button class="edit-btn-properties" onClick={handleImageClick}>Edit Profile</button>
+                        </div>
+                    </div>
+
+                    <div className='username-container-prof-settings'>
+                        <h1 className='username-properties'>Suosuo Frieren</h1>
+                    </div>
+
+                    
+                    <div className='forms-container-1'>
+
+                        <div className='forms-properties'>
+                            <label className='forms-label-properties'>First Name</label>
+                            <input className='input-properties'
+                                type='text'
+                                placeholder='First Name'
+                                value={"Suosuo"}
+                            />
+                        </div>
+
+                        <div className='forms-properties'>
+                            <label className='forms-label-properties' >Last Name</label>
+                            <input className='input-properties'
+                                type='text'
+                                placeholder='Last Name'
+                                value={"Frieren"}
+                            />
+                        </div>
+
+                        <div className='forms-properties'>
+                            <label className='forms-label-properties'>Email</label>
+                            <input className='input-properties'
+                                type='text'
+                                placeholder='Email'
+                                value={"virgojl@student-nu-moa.edu.ph"}
+                            />
+                        </div>
+                    
+                    </div>
+
+                    <div className='forms-container-2'>
+                        <div className='forms-properties'>
+                            <label className='forms-label-properties'>Employee No.</label>
+                            <input className='input-properties'
+                                type='text'
+                                placeholder='Employee No.'
+                                value={"01"}
+                            />
+                        </div>
+
+                        <div className='forms-properties'>
+                            <label class="forms-label-properties">NU Branch</label>
+                                <select class="input-properties">
+                                    <option value="default">Select a Branch</option>
+                                    <option value="manila">NU Manila</option>
+                                    <option value="moa">NU MOA</option>
+                                    <option value="laguna">NU Laguna</option>
+                                    <option value="fairview">NU Fairview</option>
+                                    <option value="baliwag">NU Baliwag</option>
+                                    <option value="dasma">NU Dasmarinas</option>
+                                    <option value="lipa">NU Lipa</option>
+                                    <option value="clark">NU Clark</option>
+                                    <option value="bacolod">NU Bacolod</option>
+                                    <option value="eastortigas">NU East Ortigas</option>
+                                    
+                                </select>
+                        </div>
+
+                        <div className='forms-properties'>
+                            <label className='forms-label-properties'>Position</label>
+                            <input className='input-properties'
+                                type='text'
+                                placeholder='Position'
+                                value={"Accounts Admin"}
+                            />
+                        </div>
+                    </div>
+
+
+
+                </div>     
+            </div>
+
+                    {/* <div className='forms-container-prof-settings'>
+                        
+                        
+
+                    </div> */}
+
+            {/* <div className='forms-properties'>
+                            <label className='forms-label-properties'>First Name</label>
+                            <input className='input-properties' 
+                                type='text'
+                                placeholder='First Name'
+                                value={"Suosuo"}
+                            />
+                        </div>
+                                
+                        <div className='forms-properties'>
+                            <label className='forms-label-properties'>Last Name</label>
+                            <input className='input-properties' 
+                                type='text'
+                                placeholder='Last Name'
+                                value={"Frieren"}
+                            />
+                        </div>
+
+                        <div className='forms-properties'>
+                            <label className='forms-label-properties'>Email</label>
+                            <input className='input-properties' 
+                                type='text'
+                                placeholder='Email'
+                                value={"virgojl@students.nu-moa.edu.ph"}
+                             />
+                        </div> */}
+
+            {/* <div class="header-cont-prof-settings">
+                    <h1 class="header-text-prof-settings">Profile Settings</h1>
+                </div> */}
+
+            {/* <div class="content-cont">
                     <div class="prof-pic-name-edit-btn-cont">
 
                         <div class="prof-pic-cont">
@@ -53,54 +190,9 @@ export default function Profile(){
                         <h1 class="user-name-prof-settings">Suosuo Frieren</h1>
                     </div>
 
-                    <div class="forms-cont-prof-settings">
+                    
 
-                        <div class="first-three-forms-cont">
-                            
-                            <div class="fname-div">
-                                <label for="fname" class='form-label'>First Name</label>
-                                <input class="forms-design"
-                                    type='text'
-                                    value={"Suosuo"}
-                                    id="'fname'"
-                                />
-                            </div> 
-
-                            <div class="lname-div">
-                                <label class='form-label'>Last Name</label>
-                                <input class="forms-design"
-                                    type='text'
-                                    value={"Frieren"}
-                                />
-                            </div> 
-
-                            <div class="email-div">
-                                <label class='form-label'>Email</label>
-                                <input class="forms-design"
-                                    type='text'
-                                    value={"virgojl@students.nu-moa.edu.ph"}
-                                />
-                            </div> 
-                        </div>
-
-                        <div class="second-three-forms-cont">
-
-                            <div>
-
-                            </div> 
-
-                            <div>
-                                
-                            </div> 
-                            
-                            <div>
-                                
-                            </div> 
-
-                        </div>
-                    </div>
-
-                </div>
+                </div> */}
 
 
                 
@@ -179,7 +271,6 @@ export default function Profile(){
                         </div>
                     </div>
                 </div> */}
-            </div>
         </>
     )
 }
