797e88daaba6e177e0c9879aaf50f875c18b9810
gialim <gianette0808@gmail.com>
2025-04-12 03:46:44 +0800

    Manage Glossary & Search Bar
    Search bar component; Search filter in Manage Glossary page [lol still subject to change kasi ang haba ng code, but pls check lol]

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,7 +1,11 @@
-export default function SearchBar(){
+import './searchbar.css'
+
+export default function SearchBar(props){
+
     return(
-        <>
-            
-        </>
+       <input type="search" 
+       className="search" 
+       placeholder={props.placeholder} 
+       onChange={props.handleChange}/>
     )
 }
