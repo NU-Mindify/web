0c4c2e6d48baa51d1c69be55c5b0089b9ffbd81a
jvimperial <113832830+jvimperial@users.noreply.github.com>
2025-05-19 18:31:52 +0800

    Branch Management, Login Bug Fix
    - Fetched branches from database
    - Fixed validations

--- src/Constants.js
+++ src/Constants.js
@@ -1,67 +1,72 @@
+import axios from "axios"
+
 export const API_URL = import.meta.env.VITE_URL
 
+axios.get(API_URL+"/getbranches")
+.then(({data}) => branches = data)
+export let branches = [
 
-export const branches = [
-  {
-    id: "moa",
-    name: "NU MOA",
-    extension: '@nu-moa.edu.ph'
-  },
-  {
-    id: "manila",
-    name: "NU Manila",
-    extension: '@nu-manila.edu.ph'
-  },
-  {
-    id: "baliwag",
-    name: "NU Baliwag",
-    extension: '@nu-baliwag.edu.ph'
-  },
-  {
-    id: "fairview",
-    name: "NU Fairview",
-    extension: '@nu-fairview.edu.ph'
-  },
-  {
-    id: "eastortigas",
-    name: "NU East-Ortigas",
-    extension: '@nu-east-ortigas.edu.ph'
-  },
-  {
-    id: "laspinas",
-    name: "NU Las Pinas",
-    extension: '@nu-las-pinas.edu.ph'
-  },
-  {
-    id: "lipa",
-    name: "NU Lipa",
-    extension: '@nu-lipa.edu.ph'
-  },
-  {
-    id: "clark",
-    name: "NU Clark",
-    extension: '@nu-clark.edu.ph'
-  },
-  {
-    id: "laguna",
-    name: "NU Laguna",
-    extension: '@nu-laguna.edu.ph'
-  },
-  {
-    id: "dasmarinas",
-    name: "NU Dasmarinas",
-    extension: '@nu-dasmarinas.edu.ph'
-  },
-  {
-    id: "bacolod",
-    name: "NU Bacolod",
-    extension: '@nu-bacolod.edu.ph'
-  },
-  {
-    id: "cebu",
-    name: "NU Cebu",
-    extension: '@nu-cebu.edu.ph'
-  }
+
+  // {
+  //   "id": "moa",
+  //   "name": "NU MOA",
+  //   "extension": "@nu-moa.edu.ph"
+  // },
+  // {
+  //   "id": "manila",
+  //   "name": "NU Manila",
+  //   "extension": "@nu-manila.edu.ph"
+  // },
+  // {
+  //   "id": "baliwag",
+  //   "name": "NU Baliwag",
+  //   "extension": "@nu-baliwag.edu.ph"
+  // },
+  // {
+  //   "id": "fairview",
+  //   "name": "NU Fairview",
+  //   "extension": "@nu-fairview.edu.ph"
+  // },
+  // {
+  //   "id": "eastortigas",
+  //   "name": "NU East-Ortigas",
+  //   "extension": "@nu-east-ortigas.edu.ph"
+  // },
+  // {
+  //   "id": "laspinas",
+  //   "name": "NU Las Pinas",
+  //   "extension": "@nu-las-pinas.edu.ph"
+  // },
+  // {
+  //   "id": "lipa",
+  //   "name": "NU Lipa",
+  //   "extension": "@nu-lipa.edu.ph"
+  // },
+  // {
+  //   "id": "clark",
+  //   "name": "NU Clark",
+  //   "extension": "@nu-clark.edu.ph"
+  // },
+  // {
+  //   "id": "laguna",
+  //   "name": "NU Laguna",
+  //   "extension": "@nu-laguna.edu.ph"
+  // },
+  // {
+  //   "id": "dasmarinas",
+  //   "name": "NU Dasmarinas",
+  //   "extension": "@nu-dasmarinas.edu.ph"
+  // },
+  // {
+  //   "id": "bacolod",
+  //   "name": "NU Bacolod",
+  //   "extension": "@nu-bacolod.edu.ph"
+  // },
+  // {
+  //   "id": "cebu",
+  //   "name": "NU Cebu",
+  //   "extension": "@nu-cebu.edu.ph"
+  // }
 ]
 
 export const categories = [
