6aa21a3c54562f49a58a866bf31754e3b24b2ee7
mcafe03 <ccmafe.lacaba@gmail.com>
2025-08-01 00:34:37 +0800

    Update landing.jsx

--- src/pages/landingpage/landing.jsx
+++ src/pages/landingpage/landing.jsx
@@ -1,4 +1,4 @@
-import "../../css/Landingpage/landing.css";
+import "../../css/landingpage/landing.css";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
 import phone1 from "../../assets/landingpage/phone1.png";
